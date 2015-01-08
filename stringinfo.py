f1f71ed055fb2695cc1ac0335e828e4a3c138aa5
Peter Wagenaar <sakartu@gmail.com>
2015-01-08 09:08:20 +0100

    Add some basic code

--- stringinfo.py
+++ stringinfo.py
@@ -1,17 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 """
 Usage:
-stringinfo
+stringinfo.py STRING
+STRING          The string of which you want information
 """
 
 from docopt import docopt
+import plugins
 
 __author__ = 'peter'
 
 
 def main():
     args = docopt(__doc__)
+    print(args)
+
+    # Find plugins
+    p = plugins.get_plugins()
+    # For each plugin, check if it's applicable and if so, run it
 
 
 if __name__ == '__main__':

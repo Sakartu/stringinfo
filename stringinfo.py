aea1f7732b27a69ef58fbfd93113c3dbd25fe686
Peter Wagenaar <sakartu@gmail.com>
2015-01-08 08:57:23 +0100

    Initial commit

---
+++ stringinfo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+"""
+Usage:
+stringinfo
+"""
+
+from docopt import docopt
+
+__author__ = 'peter'
+
+
+def main():
+    args = docopt(__doc__)
+
+
+if __name__ == '__main__':
+    main()

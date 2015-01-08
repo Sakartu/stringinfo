a029c6f4fce36693a9dee53ff8bc797890cfe71e
Peter Wagenaar <sakartu@gmail.com>
2015-01-08 12:29:47 +0100

    Use table instead of separate lines

--- plugins/basic_info_plugin.py
+++ plugins/basic_info_plugin.py
@@ -1,5 +1,6 @@
 import string
 import textwrap
+from veryprettytable import VeryPrettyTable
 from plugins import BasePlugin
 
 __author__ = 'peter'
@@ -20,9 +21,10 @@ class BasicInfoPlugin(BasePlugin):
         for s in self.args['STRING']:
             if len(self.args['STRING']) > 1:
                 result += '{0}:\n'.format(s)
-            result += 'len: {0}\n'.format(len(s))
-            result += 'number of digits: {0}\n'.format(sum(x.isdigit() for x in s))
-            result += 'number of alpha: {0}\n'.format(sum(x.isalpha() for x in s))
-            result += 'number of unprintable: {0}\n'.format(sum(x in string.printable for x in s))
+            table = VeryPrettyTable()
+            table.field_names = ['Length', '# Digits', '# Alpha', '# unprintable']
+            table.add_row((len(s), sum(x.isdigit() for x in s), sum(x.isalpha() for x in s),
+                           sum(x in string.printable for x in s)))
+            result += str(table) + '\n'
 
         return result

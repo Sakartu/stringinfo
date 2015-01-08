0a2b239d9fc809bf7d03d8c8d4458f63ef9cbe13
Peter Wagenaar <sakartu@gmail.com>
2015-01-08 12:23:36 +0100

    Add XOR plugin

---
+++ plugins/xor_plugin.py
@@ -0,0 +1,30 @@
+import string
+import textwrap
+from plugins import BasePlugin
+
+__author__ = 'peter'
+
+
+class XORPlugin(BasePlugin):
+    name = 'XORPlugin'
+    short_description = 'XOR of the hex inputs'
+    default = False
+    description = textwrap.dedent('''
+    This plugin XOR's all hexstrings from the input with eachother, in order.
+    ''')
+
+    def sentinel(self):
+        # Only parse if all inputs are valid hex strings
+        try:
+            for s in self.args['STRING']:
+                int(s, 16)
+        except ValueError:
+            return False
+        # Only parse if there are more than one string
+        return len(self.args['STRING']) > 1
+
+    def handle(self):
+        r = int(self.args['STRING'][0], 16)
+        for s in self.args['STRING'][1:]:
+            r ^= int(s, 16)
+        return hex(r)

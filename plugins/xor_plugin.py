3fa43baf66756c5728721d5ee285dbd94cbe050d
Peter Wagenaar <sakartu@gmail.com>
2015-01-26 10:18:46 +0100

    Draw pretty table and prepend inputs with 0's if necessary

--- plugins/xor_plugin.py
+++ plugins/xor_plugin.py
@@ -23,7 +23,12 @@ class XORPlugin(BasePlugin):
         return len(self.args['STRING']) > 1
 
     def handle(self):
+        max_len = max(map(len, self.args['STRING']))
+        result = '\n'.join('0x' + hex(int(x, 16))[2:].rjust(max_len - 2, '0') for x in self.args['STRING'])
+        result += '\n' + '=' * max_len + ' ^'
+
         r = int(self.args['STRING'][0], 16)
         for s in self.args['STRING'][1:]:
             r ^= int(s, 16)
-        return hex(r)[2:]
+        result += '\n' + hex(r)
+        return result
